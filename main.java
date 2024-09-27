ccb40dd6a63c209f321436cb02f50161c8d57002
Artem (Drunkslime) Babichenko <tuturu352@gmail.com>
2024-09-27 14:41:29 +0100

    first commint

---
+++ main.java
@@ -0,0 +1,10 @@
+
+public class main {
+        public static void main(String[] args) { // This is the main
+        String [] convidados = {"Ana", "Olivia", "Óscar", "Maria", "Francisco", "Pedro", "Miguel"};
+        System.out.println("Os convidados da festa são: ");
+        for (String string : convidados) {
+            System.out.println(string);
+        }
+    }
+}

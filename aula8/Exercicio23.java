f32edb27e837f6d935ff97b42212c6c489e13eba
Artem (Drunkslime) Babichenko <tuturu352@gmail.com>
2024-10-11 16:55:52 +0100

    Exercicio 23

--- aula8/Exercicio23.java
+++ aula8/Exercicio23.java
@@ -6,9 +6,11 @@ public class Exercicio23 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("\nNome: ");
-        String username = input.nextLine();
-        MensagemDeBoasVindas(username);
-        Menu();
+        String userName = input.nextLine();
+        System.out.print("Idade: ");
+        int userAge = input.nextInt();
+        MensagemDeBoasVindas(userName);
+        Menu(userAge);
         input.close();
     }
 
@@ -16,12 +18,22 @@ public class Exercicio23 {
         System.out.println(String.format("Seja bem vindo %s", name));
     }
 
-    public static void Menu() {
-        System.out.println("== Menu ==\n" +
-                "1 - Entradas\n" +
-                "2 - Prato Carne\n" +
-                "3 - Prato Peixe\n" +
-                "4 - Sobremesa\n" +
-                "5 - Sair\n");
+    public static void Menu(int age) {
+        if (age < 18) {
+            System.out.println("== Menu ==\n" +
+                    "1 - Entradas\n" +
+                    "2 - Prato Carne\n" +
+                    "3 - Prato Peixe\n" +
+                    "4 - Sobremesa\n" +
+                    "5 - Sair\n");
+        } else {
+            System.out.println("== Menu ==\n" +
+                    "1 - Entradas\n" +
+                    "2 - Prato Carne\n" +
+                    "3 - Prato Peixe\n" +
+                    "4 - Sobremesa\n" +
+                    "5 - Carta de Vinhos\n" +
+                    "6 - Sair\n");
+        }
     }
 }

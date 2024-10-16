efbe232eb5a28d0d4e7d5530c63f5c7d2a3253e2
Artem (Drunkslime) Babichenko <tuturu352@gmail.com>
2024-10-16 16:54:51 +0100

    Exercicio 25

--- aula10/Exercicio25.java
+++ aula10/Exercicio25.java
@@ -6,12 +6,12 @@ public class Exercicio25 {
     public static void main(String[] args) {
         System.out.println("");
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter your year of birth: ");
-        int yearOfBirth = input.nextInt();
-        System.out.println("Enter current year: ");
+        System.out.println("Por favor, insira o ano atual: ");
         int currentYear = input.nextInt();
+        System.out.println("Insira o seu ano de nascimento: ");
+        int yearOfBirth = input.nextInt();
         int age = calcAge(yearOfBirth, currentYear);
-        System.out.println("Your age is: " + age);
+        System.out.println("A sua idade atual é: " + age + " Anos.");
         input.close();
     }
 

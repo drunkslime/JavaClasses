f241d98c9fcd64ac00372ee7b2981a692ab1c956
Artem (Drunkslime) Babichenko <tuturu352@gmail.com>
2024-10-16 17:22:25 +0100

    Exercicio 25

--- aula10/Exercicio25.java
+++ aula10/Exercicio25.java
@@ -1,22 +1,36 @@
 package aula10;
 
 import java.util.Scanner;
+import java.util.Calendar;
 
 public class Exercicio25 {
     public static void main(String[] args) {
         System.out.println("");
         Scanner input = new Scanner(System.in);
+
         System.out.println("Por favor, insira o ano atual: ");
         int currentYear = input.nextInt();
+
         System.out.println("Insira o seu ano de nascimento: ");
         int yearOfBirth = input.nextInt();
-        int age = calcAge(yearOfBirth, currentYear);
-        System.out.println("A sua idade atual é: " + age + " Anos.");
+
+        System.out.println("public static int calcAge(int yearOfBirth, int currentYear)");
+        int age1 = calcAge(yearOfBirth, currentYear);
+        System.out.println("A sua idade atual é: " + age1 + " Anos.");
+        
+        System.out.println("public static int calcAge(int yearOfBirth)");
+        int age2 = calcAge(yearOfBirth);
+        System.out.println("A sua idade atual é: " + age2 + " Anos.");
+
         input.close();
     }
 
     public static int calcAge(int yearOfBirth, int currentYear) {
         return currentYear - yearOfBirth;
     }
-    
+
+    public static int calcAge(int yearOfBirth) {
+        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+        return  currentYear - yearOfBirth;
+    }
 }
